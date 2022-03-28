10b0dfcfedb0273579fe63d7636cee4164de1d56
Alex Simisi Nta <smsxander@gmail.com>
2022-03-28 21:33:58 +0200

    project nextJS

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Links from "../components/Links";
+
+function about() {
+  return (
+    <div>
+      <Links />
+      <h1>About NextJS Page</h1>
+    </div>
+  );
+}
+
+export default about;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Links from '../components/Links';
+
+function HomePage() {
+  return (
+    <div>
+      <Links />
+      <h1>Welcome to NextJS !</h1>
+    </div>
+  );
+}
+
+export default HomePage;
